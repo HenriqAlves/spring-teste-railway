c2b2ad8938f037e137d5e04b77927af0d4bf4e81
HenriqFonseca <henrique12095@gmail.com>
2023-08-19 18:16:15 -0300

    Criando User e Task Repositories

--- src/main/java/com/lucasangelo/todosimple/models/User.java
+++ src/main/java/com/lucasangelo/todosimple/models/User.java
@@ -1,32 +1,39 @@
 package com.lucasangelo.todosimple.models;
 
 import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 @Entity
 @Table(name = User.TABLE_NAME)
 public class User {
     public static final String TABLE_NAME = "user";
 
-    public interface createUser {}
-    public interface updateUser {}
+    public interface createUser {
+    }
 
+    public interface updateUser {
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO INCREMENT PADRÃO MYSQL
-    @Column(name = "id", unique=true)
+    @Column(name = "id", unique = true)
     private Long id;
-
 
     @Column(name = "username", length = 100, nullable = false, unique = true)
     @NotNull
@@ -34,66 +41,104 @@ public class User {
     @Size(min = 4, max = 100)
     private String username;
 
+    @JsonProperty(access = Access.WRITE_ONLY)
     @Column(name = "password", length = 50, nullable = false)
     @Size(min = 2, max = 50)
     @NotBlank
     private String password;
 
-    // private List<Task> = new ArrayList<Task>();
 
-    //cosntrutor vazio
-    public User(){
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
+
+    //*  cosntrutor vazio
+    public User() {
 
     }
 
-    // Construtor com todos os campos
-    public User(Long id, String username, String password){
+    //* Construtor com todos os campos
+    public User(Long id, String username, String password) {
         this.id = id;
         this.username = username;
         this.password = password;
 
     }
 
-
-    // GETTERS E SETTERS
-
-    public Long getId(){
+    //*  GETTERS E SETTERS
+    public Long getId() {
         return this.id;
     }
 
-    public void setId(Long id){
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getUsername(){
+    public String getUsername() {
         return this.username;
     }
 
-    public void setUsername(String username){
+    public void setUsername(String username) {
         this.username = username;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return this.password;
     }
 
-    public void setPassword(String password){
+    public void setPassword(String password) {
         this.password = password;
     }
 
+    //* Getters e setters task
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+
+
 
     @Override
-    public boolean equals (Object o){
-        if(o == this){
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+
+    // Verificações
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
             return true;
         }
-        if(! o instanceof User){
+        
+        if (obj == null) {
+            return false;
+        
+        }
+        if (!(obj instanceof User)) {
             return false;
         }
 
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(username, user.username) && Object.equals;
-    }
+        User other = (User) obj;
+        
 
+
+        if (this.id == null) {
+            if (other.id != null) {
+                return false;
+            } else if (!this.id.equals(other.id)) {
+                return false;
+            }
+
+        }
+        return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username)
+                && Objects.equals(this.password, other.password);
+
+    }
 
 }
---
+++ src/main/java/com/lucasangelo/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,28 @@
+package com.lucasangelo.todosimple.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.lucasangelo.todosimple.models.Task;
+
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    
+    Optional<Task> findById(Long id);
+
+
+    //* ORM JAVA SPRING */
+    // @Query(value = "SELECT task FROM Task task WHERE task.user.id = : id")
+    // List<Task> findByUser_id(@Param("id") Long id);
+
+
+    //* SQL PADRÃO */
+    // @Query(value = "SELECT * FROM task task WHERE task.user_id = :id", nativeQuery = true)
+    // List<Task> findByUser_Id(@Param("id") Long id);
+}
