6a1ceebd318f71c2abc5c4da7f0996309a43e75e
HenriqFonseca <henrique12095@gmail.com>
2023-08-25 13:13:15 -0300

    TaskController e FindAllTasks

--- src/main/java/com/lucasangelo/todosimple/controllers/UserController.java
+++ src/main/java/com/lucasangelo/todosimple/controllers/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
